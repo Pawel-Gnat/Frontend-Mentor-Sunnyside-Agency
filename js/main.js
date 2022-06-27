91e827ce9aa1484d1583bc9108fe2e07e55ddb4c
Paweł <pawel_gnat@o2.pl>
2022-06-27 22:31:36 +0200

    added html css js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector('.nav-mobile__btn')
+const toggleMenu = document.querySelector('.menu')
+
+menuBtn.addEventListener('click', () => {
+	menuBtn.classList.toggle('menu')
+})
