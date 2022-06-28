dc0b2641d94c500b15d546c3a42a92de0c4991bc
Paweł <pawel_gnat@o2.pl>
2022-06-28 21:18:39 +0200

    added html css js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const menuBtn = document.querySelector('.nav-mobile__btn')
-const toggleMenu = document.querySelector('.menu')
+const hamburger = document.querySelector('.menu')
+const showLinks = document.querySelector('.nav-mobile--links')
+const activeLinks = document.querySelector('.active')
 
 menuBtn.addEventListener('click', () => {
 	menuBtn.classList.toggle('menu')
+	showLinks.classList.toggle('active')
 })
